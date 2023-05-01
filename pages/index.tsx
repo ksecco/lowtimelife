26de2d67c37661f02d3851072a3994d6d03591b6
dca btc <dcabtc@lowtime.life>
2023-05-01 08:22:44 -0700

    adding title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
 import { Inter } from 'next/font/google'
 import Genesis from './genesis'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
+  return (<>
+    <Head>
+      <title>Low Time Life</title>
+      <meta name="description" content="What is Bitcoin?" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
     <Genesis />
+  </>
   )
 }
