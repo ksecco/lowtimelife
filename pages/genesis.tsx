205466a40814c7379d9073cf22718189e7a7c2e4
dca btc <dcabtc@lowtime.life>
2023-05-01 21:54:33 -0700

    gc -m updating secondary button

--- pages/genesis.tsx
+++ pages/genesis.tsx
@@ -177,13 +177,13 @@ export default function Genesis() {
                         ? "I'm beginning to understand..."
                         : "I accept"}
                     </button>
-                    {showReferral && (
+                    {currentModalCategory === "swan" && (
                       <button
                         type="button"
                         onClick={closeReferralModal}
-                        className="inline-flex justify-center rounded-md border border-transparent bg-gray-100 px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-500 focus-visible:ring-offset-2 ml-2"
+                        className="inline-flex justify-center rounded-md border border-transparent bg-gray-100 px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 ml-2"
                       >
-                        {"I'm not ready to thrive"}
+                        I do not value my time
                       </button>
                     )}
                   </div>
