91f5745b933ae7dcd5a25410655e927c037e6988
dca btc <dcabtc@lowtime.life>
2023-05-03 01:00:03 -0700

    adding color themes

---
+++ styles/themes.js
@@ -0,0 +1,20 @@
+export const themes = {
+  dark: {
+    primary: "black",
+    headerText: "white",
+    bodyText: "gray-300",
+    orange: "orange-500",
+    linkPrimary: "orange-500",
+    linkSecondary: "blue-100",
+  },
+  light: {
+    primary: "white",
+    headerText: "gray-900",
+    bodyText: "gray-500",
+    orange: "orange-200",
+    linkPrimary: "blue-900",
+    linkSecondary: "orange-500",
+  },
+};
+
+export default themes;
